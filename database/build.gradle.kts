44aae71f0115a6a3e2eecec69ce66e1be0b634f9
Fahman Haqqi <fahmanhq@gmail.com>
2024-09-26 17:21:08 +0700

    Clean up dependencies on :database module

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -45,9 +45,6 @@ dependencies {
     api(libs.room.ktx)
     ksp(libs.room.compiler)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
